0fd80a118fe0c2e46f8f5c586ea81cc96483fda4
Rodrigo <rodrigodot42@gmail.com>
2018-11-03 18:19:52 -0300

    Added some error translation to mongoose

--- back/src/config/database.js
+++ back/src/config/database.js
@@ -4,3 +4,4 @@ mongoose.Promise = global.Promise
 module.exports = mongoose.connect('mongodb://localhost/app2')
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório"
+mongoose.Error.messages.Number.min = "O '{VALUE}' e menor do que o valor minimo"
