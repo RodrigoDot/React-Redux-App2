51da93b9a28d394273f7f8d094b7b691c06e170e
Rodrigo <rodrigodot42@gmail.com>
2018-11-03 17:35:10 -0300

    Mongoose service done

---
+++ back/src/api/billingCycle/billingCycleService.js
@@ -0,0 +1,15 @@
+const BillingCycle = require('./billingCycle')
+
+BillingCycle.methods([
+  'get',
+  'post',
+  'delete',
+  'put'
+])
+
+BillingCycle.updateOptions({
+  new: true,
+  runValidators: true
+})
+
+module.exports = BillingCycle
